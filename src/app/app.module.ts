4b32a817349f3df8e6a92c4b3115ae781b9f8f75
3p4r4 <mendoza124302@gmail.com>
2023-04-09 20:23:59 -0500

    Implementado Material Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { BasicFormComponent } from './components/basic-form/basic-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import {MatSliderModule} from '@angular/material/slider'; 
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +22,17 @@ import { BasicFormComponent } from './components/basic-form/basic-form.component
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatSliderModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/basic-form/basic-form.component.ts
+++ src/app/components/basic-form/basic-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from "@angular/forms";
+import { FormControl, Validators, FormGroup, FormBuilder } from "@angular/forms";
+
 
 @Component({
   selector: 'app-basic-form',
@@ -8,7 +9,30 @@ import { FormControl, Validators } from "@angular/forms";
 })
 export class BasicFormComponent {
 
-  nameField = new FormControl('', Validators.required);
+  form!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ){
+    this.builderForm();
+  }
+  private builderForm() {
+      this.form = this.formBuilder.group({
+      fullName:this.formBuilder.group({
+        name: ['', Validators.required],
+        last: ['', Validators.required],
+      }),
+      email: ['', [Validators.required, Validators.email]],
+      phone: [''],
+      date: [''],
+      passwd: [''],
+      color: ['#000'],
+      range: [''],
+      search: ['']
+    });
+  }
+
+  /* nameField = new FormControl('', Validators.required);
   emailField = new FormControl();
   phoneField = new FormControl();
   dateField = new FormControl();
@@ -16,9 +40,58 @@ export class BasicFormComponent {
   colorField = new FormControl('#000');
   rangeField = new FormControl();
   searchField = new FormControl();
+ */
+/**Creando Getter para FormControl**/
+
+get nameField(){
+  return this.form.get('fullName.name') as FormControl;
+}
+get lastField(){
+  return this.form.get('fullName.last') as FormControl;
+}
+get emailField(){
+  return this.form.get('email') as FormControl;
+}
+get phoneField(){
+  return this.form.get('phone') as FormControl;
+}
+get dateField(){
+  return this.form.get('date') as FormControl;
+}
+get passwdField(){
+  return this.form.get('passwd') as FormControl;
+}
+get colorField(){
+  return this.form.get('color') as FormControl;
+}
+get rangeField(){
+  return this.form.get('range') as FormControl;
+}
+get searchField(){
+  return this.form.get('search') as FormControl;
+}
+save(event:Event){
+  console.log(this.form.value);
+  
+}
+/**********/
 
   getBtn(){
     console.log(this.nameField.value)
   }
+  get isnameFieldValid(){
+    return this.nameField.touched && this.nameField.valid
+  }
+  get isnameFieldinValid(){
+    return this.nameField.touched && this.nameField.invalid
+  }
+
+  get isemailFieldValid(){
+    return this.emailField.touched && this.emailField.valid
+  }
+  get isemailFieldinValid(){
+    return this.emailField.touched && this.emailField.hasError('required')
+  }
+
 
 }
